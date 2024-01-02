ae50011eb29b751e7750ba10c356b5e5380f1576
dan <79108442+sxverance@users.noreply.github.com>
2024-01-02 21:24:23 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Trigger the error effect after a delay
-setTimeout(() => {
-    const errorContainer = document.getElementById('errorContainer');
-    errorContainer.style.display = 'block';
-  }, 1000); // Delay in milliseconds
-  
