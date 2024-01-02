c5fb0bd4005e670ae3a54a10832c22ea25c434a4
dan <79108442+sxverance@users.noreply.github.com>
2024-01-02 21:19:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Trigger the error effect after a delay
+setTimeout(() => {
+    const errorContainer = document.getElementById('errorContainer');
+    errorContainer.style.display = 'block';
+  }, 1000); // Delay in milliseconds
+  
